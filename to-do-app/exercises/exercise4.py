ab4c97c0470d94d2726e03ac9c196cc40d38428b
Harika Naidu <naidu.harika00@gmail.com>
2023-08-09 15:38:07 -0400

    Delete to-do-app/exercises directory

--- to-do-app/exercises/exercise4.py
+++
@@ -1,3 +0,0 @@
-while True:
-    user_name = input("What is your name? ")
-    print(user_name.capitalize())
--- to-do-app/exercises/exercise6.py
+++
@@ -1,6 +0,0 @@
-filenames = ['doc.txt', 'report.txt', 'presentation.txt']
-
-for filename in filenames:
-    file = open(f'../files/{filename}', 'w')
-    file.write('Hello')
-    file.close()
