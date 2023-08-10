1608181904686221103165ecb69ad88cbf863d75
Harika Naidu <naidu.harika00@gmail.com>
2023-08-10 12:37:40 -0400

    implemented complete and exit button

--- to-do-app/gui.py
+++ to-do-app/gui.py
@@ -7,11 +7,15 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos_from_file(), key='todos',
                       enable_events=True, size=[45, 10])
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button('Exit')
 
+layout = [[label],
+          [input_box, add_button],
+          [list_box, edit_button, complete_button],
+          [exit_button]]
 window = sg.Window('My To-Do App',
-                   layout=[[label],
-                           [input_box, add_button],
-                           [list_box, edit_button]],
+                   layout=layout,
                    font=('Helvetica', 20))
 
 while True:
@@ -34,6 +38,15 @@ while True:
             todos[index] = new_todo + "\n"
             functions.write_to_file(todos)
             window['todos'].update(values=todos)
+        case "Complete":
+            todos_to_complete = values["todos"][0]
+            todos = functions.get_todos_from_file()
+            todos.remove(todos_to_complete)
+            functions.write_to_file(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value='')
+        case "Exit":
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
