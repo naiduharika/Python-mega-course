e033852b22b89be27d600b0fb4fb5e28f0ade92e
Harika Naidu <naidu.harika00@gmail.com>
2023-09-01 13:31:19 -0400

    add description on the new columns

--- portfolio/main.py
+++ portfolio/main.py
@@ -5,15 +5,22 @@ st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
-with col1:
-    st.image("images/photo.png")
+with st.container():
+    with col1:
+        st.image("images/photo.png")
 
-with col2:
-    st.title("Harika Naidu")
-    content = """
-    Hey there! I'm Harika, a passionate software developer with a penchant for backend development. Python is my 
-    language of choice, but I'm also on a journey exploring GoLang. When I'm not immersed in coding, you can find me 
-    diving into a good book, catching up on my favorite TV shows, or indulging in the rhythm of a volleyball game.
-    Looking for a collaborative tech enthusiast with diverse interests? You've found one. Let's chat!
+    with col2:
+        st.title("Harika Naidu")
+        content = """
+        Hey there! I'm Harika, a passionate software developer with a penchant for backend development. Python is my 
+        language of choice, but I'm also on a journey exploring GoLang. When I'm not immersed in coding, you can find me 
+        diving into a good book, catching up on my favorite TV shows, or indulging in the rhythm of a volleyball game.
+        Looking for a collaborative tech enthusiast with diverse interests? You've found one. Let's chat!
+        """
+        st.info(content)
+
+with st.container():
+    content2 = """
+    Below you can find some of the apps I have built with Python. Feel free to contact me!
     """
-    st.info(content)
+    st.write(content2)
