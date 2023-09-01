a121afb994dab064b55a8d9f20bb97b0d910ef42
Harika Naidu <naidu.harika00@gmail.com>
2023-09-01 14:20:58 -0400

    add app description, image and link

--- portfolio/main.py
+++ portfolio/main.py
@@ -23,14 +23,20 @@ Below you can find some of the apps I have built with Python. Feel free to conta
 """
 st.write(content2)
 
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv("data.csv", sep=";")
 
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row['image']}")
+        st.write(f"[Source Code]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row['image']}")
+        st.write(f"[Source Code]({row['url']})")
