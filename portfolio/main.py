36773ac91c6081d2493867e2371ed0288ab9108e
Harika Naidu <naidu.harika00@gmail.com>
2023-09-01 14:11:49 -0400

    render the app titles in two columns

--- portfolio/main.py
+++ portfolio/main.py
@@ -1,26 +1,36 @@
 import streamlit as st
-
+import pandas
 
 st.set_page_config(layout="wide")
 
 col1, col2 = st.columns(2)
 
-with st.container():
-    with col1:
-        st.image("images/photo.png")
+with col1:
+    st.image("images/photo.png")
 
-    with col2:
-        st.title("Harika Naidu")
-        content = """
-        Hey there! I'm Harika, a passionate software developer with a penchant for backend development. Python is my 
-        language of choice, but I'm also on a journey exploring GoLang. When I'm not immersed in coding, you can find me 
-        diving into a good book, catching up on my favorite TV shows, or indulging in the rhythm of a volleyball game.
-        Looking for a collaborative tech enthusiast with diverse interests? You've found one. Let's chat!
-        """
-        st.info(content)
-
-with st.container():
-    content2 = """
-    Below you can find some of the apps I have built with Python. Feel free to contact me!
+with col2:
+    st.title("Harika Naidu")
+    content = """
+    Hey there! I'm Harika, a passionate software developer with a penchant for backend development. Python is my 
+    language of choice, but I'm also on a journey exploring GoLang. When I'm not immersed in coding, you can find me 
+    diving into a good book, catching up on my favorite TV shows, or indulging in the rhythm of a volleyball game.
+    Looking for a collaborative tech enthusiast with diverse interests? You've found one. Let's chat!
     """
-    st.write(content2)
+    st.info(content)
+
+content2 = """
+Below you can find some of the apps I have built with Python. Feel free to contact me!
+"""
+st.write(content2)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
