81f8e9d5f9991430fa067d257da7219454453d90
Harika Naidu <naidu.harika00@gmail.com>
2023-09-07 11:08:50 -0400

    add more pages for each topic

--- pdf-template/main.py
+++ pdf-template/main.py
@@ -8,9 +8,11 @@ df = pd.read_csv("topics.csv")
 for index, row in df.iterrows():
     pdf.add_page()
 
-    pdf.set_font(family="Times", style="B", size=12)
+    pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100, 100, 100)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
     pdf.line(10, 21, 200, 21)
 
+    for i in range(row["Pages"]):
+        pdf.add_page()
 pdf.output("output.pdf")
