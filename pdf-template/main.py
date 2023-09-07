588f1a850e36a3a809f4075bf94e8df94b5d8018
Harika Naidu <naidu.harika00@gmail.com>
2023-09-07 11:02:07 -0400

    pdf template generator

---
+++ pdf-template/main.py
@@ -0,0 +1,16 @@
+from fpdf import FPDF
+import pandas as pd
+
+pdf = FPDF(orientation="P", unit="mm", format="A4")
+
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+
+    pdf.set_font(family="Times", style="B", size=12)
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
+    pdf.line(10, 21, 200, 21)
+
+pdf.output("output.pdf")
